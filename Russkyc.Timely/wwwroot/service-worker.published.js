7b647934533f075372f9cfbb87c41d5ec7d9453b
russkyc <jrcc1023@gmail.com>
2025-01-11 20:54:54 +0800

    :bug: fix service worker caching on github pages

--- Russkyc.Timely/wwwroot/service-worker.published.js
+++ Russkyc.Timely/wwwroot/service-worker.published.js
@@ -43,22 +43,15 @@ async function onActivate(event) {
 async function onFetch(event) {
     let cachedResponse = null;
     if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
+        // For all navigation requests, try to serve index.html from cache,
+        // unless that request is for an offline resource.
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
+        const shouldServeIndexHtml = event.request.mode === 'navigate'
+            && !manifestUrlList.some(url => url === event.request.url);
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
-
-        if (cachedResponse && cachedResponse.redirected) {
-            cachedResponse = new Response(cachedResponse.body,
-                {
-                    headers: cachedResponse.headers,
-                    status: cachedResponse.status,
-                    statusText: cachedResponse.statusText
-                });
-        }
     }
 
     return cachedResponse || fetch(event.request);
