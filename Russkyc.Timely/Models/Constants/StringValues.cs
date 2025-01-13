a1a6af48e289de9712025866aa0bc99be5c53e51
russkyc <jrcc1023@gmail.com>
2025-01-13 17:21:07 +0800

    :lipstick: Add version info to interface

--- Russkyc.Timely/Models/Constants/StringValues.cs
+++ Russkyc.Timely/Models/Constants/StringValues.cs
@@ -2,6 +2,9 @@ namespace Russkyc.Timely.Models.Constants;
 
 public static class StringValues
 {
+    // AppVersion
+    public const string AppVersion = "1.2   .0 Stable";
+    
     // LocalStorage
     public const string ClockedInStoreId = "local_clocked_in";
     public const string ClockedInTimestampStoreId = "local_clocked_in_timestamp";
