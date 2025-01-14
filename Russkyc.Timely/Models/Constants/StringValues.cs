8e062097748ce3b034b8bb580a27d98f71576938
russkyc <jrcc1023@gmail.com>
2025-01-14 18:07:41 +0800

    :recycle: Disable updater on desktop displays, fix updater prompt not appearing even after an update is available.

--- Russkyc.Timely/Models/Constants/StringValues.cs
+++ Russkyc.Timely/Models/Constants/StringValues.cs
@@ -3,7 +3,7 @@ namespace Russkyc.Timely.Models.Constants;
 public static class StringValues
 {
     // AppVersion
-    public const string AppVersion = "1.2.3 Stable";
+    public const string AppVersion = "1.2.4 Stable";
     
     // LocalStorage
     public const string ClockedInStoreId = "local_clocked_in";
