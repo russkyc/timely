440f7b15de60411031832a28914c6d0aa2f26cac
russkyc <jrcc1023@gmail.com>
2025-01-15 16:38:44 +0800

    :bookmark: Bump version

--- Russkyc.Timely/Models/Constants/StringValues.cs
+++ Russkyc.Timely/Models/Constants/StringValues.cs
@@ -3,7 +3,7 @@ namespace Russkyc.Timely.Models.Constants;
 public static class StringValues
 {
     // AppVersion
-    public const string AppVersion = "1.2.4 Stable";
+    public const string AppVersion = "1.3.0 Stable";
     
     // LocalStorage
     public const string ClockedInStoreId = "local_clocked_in";
